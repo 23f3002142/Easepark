1791608ad93d5c9244cac8d8ed019349fd5b1ea7
kshitijnig24 <kshitij.3001@gmail.com>
2025-09-01 12:54:56 +0530

    render ready

--- app.py
+++ app.py
@@ -9,13 +9,16 @@ from controllers.user_routes import user_blueprint
 from controllers.auth_routes import auth
 import os
 from flask_mail import Mail
-#http://127.0.0.1:5000
+from dotenv import load_dotenv
+load_dotenv()
+
 
 app=Flask(__name__)
-app.config['SECRET_KEY']= 'kdmn@1924'
+
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'easepark_new.sqlite3')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
@@ -29,11 +32,12 @@ login_manager.login_view = 'auth.login'# type: ignore
 def load_user(user_id):
     return db.session.get(Users, int(user_id))
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = "kshitij.3001@gmail.com"      
-app.config['MAIL_PASSWORD'] = "cbff latm dyho dhtu"   
+app.config['MAIL_SERVER'] = os.getenv("MAIL_SERVER")
+app.config['MAIL_PORT'] = int(os.getenv("MAIL_PORT",587))
+app.config['MAIL_USE_TLS'] = os.getenv("MAIL_USE_TLS") == "True"
+app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
+app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
+  
 
 mail = Mail(app)  # initialize Mail with app
 mail.init_app(app)
