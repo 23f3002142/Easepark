5cf26792069232155af36362ad38f0aa013479f4
kshitijnig24 <kshitij.3001@gmail.com>
2025-10-01 10:38:12 +0530

    mail issue

--- app.py
+++ app.py
@@ -76,8 +76,11 @@ oauth.register(
 
 #flask_mail 
 app.config['MAIL_SERVER'] = os.getenv("MAIL_SERVER")
-app.config['MAIL_PORT'] = int(os.getenv("MAIL_PORT",587))
-app.config['MAIL_USE_TLS'] = os.getenv("MAIL_USE_TLS") == "True"
+# app.config['MAIL_PORT'] = int(os.getenv("MAIL_PORT",587))
+# app.config['MAIL_USE_TLS'] = os.getenv("MAIL_USE_TLS") == "True"
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
 app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
 mail.init_app(app)
